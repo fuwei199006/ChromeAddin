896035084e1f6d2b36195173076781bfbaf88105
fuwei16 <fuwei16@centalin.com.cn>
2016-07-22 10:04:21 +0800

    remove ad

---
+++ RemoveAd/background.js
@@ -0,0 +1,14 @@
+﻿
+$(function(){
+	var ids=["ad_b1","banner_right","cpro_u2392861","ad_b2"];
+var cls=["couponSlide","pvs"];
+ 
+$.each(ids, function(index, val) {
+     /* iterate through array or object */
+     $("#"+val).remove();
+});
+$.each(cls, function(index, val) {
+     /* iterate through array or object */
+     $("."+val).remove();
+});
+})
---
+++ RemoveAd/content_script.js
@@ -0,0 +1,17 @@
+﻿
+$(function(){
+    $.post("data.json",function(res){
+        console.log(res);
+    });
+    var ids=["ad_b1","banner_right","cpro_u2392861","ad_b2","clubHeader_panelNews","QQcom_all_Width1:1"];
+var cls=["couponSlide","pvs","a4content"];
+ 
+$.each(ids, function(index, val) {
+     /* iterate through array or object */
+     $("#"+val).remove();
+});
+$.each(cls, function(index, val) {
+     /* iterate through array or object */
+     $("."+val).remove();
+});
+})
